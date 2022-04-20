43875b37da1fc3584f41aec75d5747bdcb04f9c2
Jonathan Smoley <67881240+JT2M0L3Y@users.noreply.github.com>
2022-04-20 15:21:20 -0700

    Intro/Name panels + respective unit tests to main (#43)
    * title window, name window started, some testing
    
    * root class tester, nav to game screen added
    
    * all components in intro/name, need formatting
    
    * migrated temp.java root into yahtzee.java
    
    * name fields show up now :)
    
    * names can be sent to game panel
    
    * unit tests added for nameWindow
    
    * frame title set

--- src/main/java/edu/gonzaga/Windows/TitleWindow.java
+++ src/main/java/edu/gonzaga/Windows/TitleWindow.java
@@ -1,31 +1,87 @@
 package edu.gonzaga.Windows;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class TitleWindow extends JPanel{
-    
+    JFrame window;
+    NameWindow nextPanel;
+    JPanel contentCenter;
+    JPanel contentSouth;
     JLabel yahtzeeTitle;
+    JLabel boxLabel;
     Integer[] numPlayerOptions = {1, 2, 3, 4, 5, 6, 7, 8};
     JComboBox<Integer> numPlayerDropBox = new JComboBox<>(numPlayerOptions);
-    JButton toGameWindow;
-    //JButton toSettingsWindow;
+    JButton toNameWindow;
+    DefaultListCellRenderer renderer;
 
     public TitleWindow(){
-        this.setLayout(null);
-        yahtzeeTitle = new JLabel("Yahtzee");
-        toGameWindow = new JButton("Play");
-        //toSettingsWindow = new JButton("Settings");
+        JPanel spacer = new JPanel();
+        spacer.setPreferredSize(new Dimension(800, 200));
+        add(spacer, BorderLayout.NORTH);
+
+        setCenterContent();
+        add(contentCenter, BorderLayout.CENTER);
+
+        setSouthContent();
+        add(contentSouth, BorderLayout.SOUTH);
     }
 
-    //to be set after all screens created in Yahtzee
-    public void setSwitchButton(JPanel gameWindow){
-        toGameWindow.addActionListener(new ActionListener(){
+    private void setCenterContent(){
+        contentCenter = new JPanel();
+        contentCenter.setLayout(new BoxLayout(contentCenter, BoxLayout.Y_AXIS));
+        contentCenter.setPreferredSize(new Dimension(450, 300));
+
+        yahtzeeTitle = new JLabel("Yahtzee");
+        yahtzeeTitle.setFont(new Font("Serif", Font.PLAIN, 120));
+        
+        contentCenter.add(yahtzeeTitle);
+    
+        numPlayerDropBox.setFont(new Font("Serif", Font.PLAIN, 30));
+        numPlayerDropBox.setMaximumSize(new Dimension(300, 30));
+        renderer = new DefaultListCellRenderer();
+        renderer.setHorizontalAlignment(DefaultListCellRenderer.CENTER);
+        numPlayerDropBox.setRenderer(renderer);
+
+        contentCenter.add(numPlayerDropBox);
+
+        boxLabel = new JLabel("Number of Players");
+        boxLabel.setFont(new Font("Serif", Font.PLAIN, 30));
+        
+        contentCenter.add(boxLabel);
+    }
+
+    private void setSouthContent(){
+        contentSouth = new JPanel(new FlowLayout(FlowLayout.CENTER, 90, 40));
+        contentSouth.setPreferredSize(new Dimension(450, 150));
+
+        toNameWindow = new JButton("Start");
+        toNameWindow.setFont(new Font("Serif", Font.PLAIN, 30));
+        
+        contentSouth.add(toNameWindow);
+    }
+
+    public void setSwitchButton(JFrame main){
+        window = main;
+        toNameWindow.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent event){
-                gameWindow.setVisible(true);
+                NameWindow nameWindow = new NameWindow(window, numPlayerDropBox.getSelectedIndex() + 1);
+                nameWindow.setSwitchButton(window);
+                nextPanel = nameWindow;
+                main.add(nameWindow);
+                nameWindow.setVisible(true);
                 setVisible(false);
             }
         });
     }
 
+    // to be used for unit testing purposes
+    public JPanel getContentCenter() { return contentCenter; }
+    public JPanel getContentSouth() { return contentSouth; }
+    public JLabel getTitle() { return yahtzeeTitle; }
+    public JLabel getBoxLabel() { return boxLabel; }
+    public JComboBox<Integer> getNumPlayerDropBox() { return numPlayerDropBox; }
+    public JButton getToNameWindow() { return toNameWindow; }
+    public JPanel getNamePanel() { return nextPanel; }
 }
---
+++ src/test/java/edu/gonzaga/TitleTest.java
@@ -0,0 +1,38 @@
+package edu.gonzaga;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import edu.gonzaga.Windows.*;
+
+public class TitleTest {
+    @Test
+    void testPanelInit() {
+        TitleWindow titleWindow = new TitleWindow();
+        Assertions.assertNotNull(titleWindow);
+    }
+
+    @Test
+    void testTitleInit() {
+        TitleWindow titleWindow = new TitleWindow();
+        Assertions.assertEquals("Yahtzee", titleWindow.getTitle().getText());
+    }
+
+    @Test
+    void testButtonInit() {
+        TitleWindow titleWindow = new TitleWindow();
+        Assertions.assertEquals("Start", titleWindow.getToNameWindow().getText());
+    }
+
+    @Test
+    void testDropBoxInit() {
+        TitleWindow titleWindow = new TitleWindow();
+        Assertions.assertEquals(0, titleWindow.getNumPlayerDropBox().getSelectedIndex());
+    }
+
+    @Test
+    void testDropBoxUse() {
+        TitleWindow titleWindow = new TitleWindow();
+        titleWindow.getNumPlayerDropBox().setSelectedIndex(7);
+        Assertions.assertEquals(7, titleWindow.getNumPlayerDropBox().getSelectedIndex());
+    }
+}
