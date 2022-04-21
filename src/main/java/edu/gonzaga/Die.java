fd68c6fc85c5dd562aa3b3e541132caebfee6067
Hailey Boe <hboe@zagmail.gonzaga.edu>
2022-04-21 11:29:24 -0700

    die - hailey

--- src/main/java/edu/gonzaga/Die.java
+++ src/main/java/edu/gonzaga/Die.java
@@ -5,90 +5,93 @@ package edu.gonzaga;
 * Class for a Die used in Yahtzee.
 */
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
 /** Class to store the state of a single die. */
 public class Die implements Comparable<Die> {
 
-    private static Integer sideUp; // Current die 'value' in range 1..numSides
-    private static Integer numSides; // Sides on the die (should be 1...INF integer)
-    private static Boolean locked;
+    private Integer sideUp; // Current die 'value' in range 1..numSides
+    private Integer numSides; // Sides on the die (should be 1...INF integer)
+    private Boolean locked;
     private static final Integer DEFAULT_NUM_SIDES = 6;
     private static final Integer DEFAULT_SIDE_UP = 1;
-    private DieView2 viewDie;
-
-    public void setSideUp(int chosenSide) { sideUp = chosenSide; }
-    public static int getSideUp(){
-        return sideUp;
-    }
-    public static int getNumSides() { return numSides; }
-    public static boolean getLockedStatus() { return locked; }
+    private DieView2 dieView;
 
     public Die() {
         this.numSides = DEFAULT_NUM_SIDES;
         this.sideUp = DEFAULT_SIDE_UP;
         this.locked = true;
-        viewDie = new DieView2();
-        viewDie.configureView();
-        Die[] die = new Die[6];
+        dieView = new DieView2(sideUp, numSides);
+        dieView.configureView();
+        dieView.getRadioButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateLockedState();
+            }
+        });
     }
 
     public Die(Integer numSides) {
         this.numSides = numSides;
         this.sideUp = DEFAULT_SIDE_UP;
         this.locked = true;
-        viewDie = new DieView2();
-        viewDie.configureView();
-        Die[] die = new Die[6];
+        dieView = new DieView2(sideUp, numSides);
+        dieView.configureView();
+        dieView.getRadioButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateLockedState();
+            }
+        });
     }
 
     public Die(Integer numSides, Integer startingSide) {
         this.numSides = numSides;
         this.sideUp = startingSide;
         this.locked = true;
-        viewDie = new DieView2();
-        viewDie.configureView();
-        Die[] die = new Die[6];
+        dieView = new DieView2(sideUp, numSides);
+        dieView.configureView();
+        dieView.getRadioButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateLockedState();
+            }
+        });
     }
 
     /** Rolls the die once, getting new random value. */
     public void roll() {
-        Random rand = new Random();
-        this.sideUp = rand.nextInt(this.numSides) + 1;
-
-        for(int i = 0; i < 6; i++) {
-            if(!viewDie.button.isSelected()) {
-                unlock();
-                this.sideUp = rand.nextInt(this.numSides) + 1;
-                viewDie.updateDieAppearance(getSideUp());
-            }
+        if(!locked) {
+            Random rand = new Random();
+            this.sideUp = rand.nextInt(this.numSides);
+            this.sideUp = rand.nextInt(this.numSides);
+            dieView.updateDieAppearance(this.sideUp);
         }
-        /************
-        needs to call a lock state ie lock(i) for when all three rolls are complete, i think this will be in hand?
-        ************/
     }
 
-    public void lock(int i) {
-        locked = true;
-        updateLockedState(i);
-    }
-
-    public void unlock() {
-        locked = false;
-    }
-
-    public void updateLockedState(int i) {
-        if(locked) {
-            viewDie.button.setVisible(false);
-        } else {
-            viewDie.button.setVisible(true);
+    public void updateLockedState() {
+        if(dieView.button.isSelected()) {
+            locked = true;
+        }
+        else {
+            locked = false;
         }
     }
 
     public void setValue(int dieFace) {
-        viewDie.updateDieAppearance(1);
+        dieView.updateDieAppearance(dieFace);
     }
 
+    public void setButtonVisibility() {
+        dieView.button.setVisible(false);
+    }
+
+    public JPanel getView() {
+        return dieView;
+    }
 
     /**
     * Provides the ability to convert the Die object into a string. representation, both with
@@ -112,7 +115,6 @@ public class Die implements Comparable<Die> {
     */
     @Override
     public int compareTo(Die otherDie) {
-        int myvar_aslkdfj = 2;
         return this.sideUp.compareTo(otherDie.sideUp);
     }
 }
