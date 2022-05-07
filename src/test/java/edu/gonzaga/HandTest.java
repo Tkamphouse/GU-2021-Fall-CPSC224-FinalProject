5494d80ffa4fa310e20a0bd56a09e16c53652eb9
Tyler Kamphouse <71041346+Tkamphouse@users.noreply.github.com>
2022-05-06 22:03:32 -0700

    Separated Assertions

--- src/test/java/edu/gonzaga/HandTest.java
+++ src/test/java/edu/gonzaga/HandTest.java
@@ -8,11 +8,16 @@ import org.junit.jupiter.api.Test;
 public class HandTest {
 
     @Test
-    void testHandConstructor() {
+    void testHandConstructorSize() {
         int expectedSize = 5;
-        int expectedFill = 1;
         Hand hand = new Hand();
         assertEquals(hand.getSize(), expectedSize);
+    }
+
+    @Test
+    void testHandConstructorFill() {
+        int expectedFill = 1;
+        Hand hand = new Hand();
         for (int i = 0; i < hand.getSize(); ++i)
             assertEquals(hand.returnValue(i), expectedFill);
     }
@@ -21,7 +26,6 @@ public class HandTest {
     void testHandRollNewKeepAll() {
         Hand hand = new Hand();
         int expectedFill = 1;
-        //Test keep all
         for (int i = 0; i < hand.getSize(); ++i)
             hand.getDie(i).changeLockedState();
         hand.RollNewHand();
@@ -42,22 +46,21 @@ public class HandTest {
     }
 
     @Test
-    void testHandSum() {
+    void testHandSum1() {
         Hand hand1 = new Hand();
-        Hand hand2 = new Hand();
-        Hand hand3 = new Hand();
         Integer expectedSum1 = 15;
-        Integer expectedSum2 = 10;
-        Integer expectedSum3 = 25;
         for (int i = 0; i < hand1.getSize(); ++i)
             hand1.getDie(i).setValue(3);
         assertEquals(hand1.sum(), expectedSum1);
-        for (int i = 0; i < hand1.getSize(); ++i)
+    }
+
+    @Test
+    void testHandSum2() {
+        Hand hand2 = new Hand();
+        Integer expectedSum2 = 10;
+        for (int i = 0; i < hand2.getSize(); ++i)
             hand2.getDie(i).setValue(2);
         assertEquals(hand2.sum(), expectedSum2);
-        for (int i = 0; i < hand1.getSize(); ++i)
-            hand3.getDie(i).setValue(5);
-        assertEquals(hand3.sum(), expectedSum3);
     }
 
     @Test
@@ -69,12 +72,18 @@ public class HandTest {
     }
 
     @Test
-    void testHandIsTurnOver() {
+    void testHandIsTurnOverFalse() {
         Hand hand = new Hand();
         hand.RollNewHand();
-        assertEquals(false, hand.isTurnOver());
         hand.RollNewHand();
         assertEquals(false, hand.isTurnOver());
+    }
+
+    @Test
+    void testHandIsTurnOverTrue() {
+        Hand hand = new Hand();
+        hand.RollNewHand();
+        hand.RollNewHand();
         hand.RollNewHand();
         assertEquals(true, hand.isTurnOver());
     }
@@ -90,11 +99,8 @@ public class HandTest {
     void testHandReset() {
         Hand hand = new Hand();
         Hand expectedHand = new Hand();
-        int expectedFill = 3;
         for (int i = 0; i < hand.getSize(); ++i)
             hand.getDie(i).setValue(3);
-        for (int i = 0; i < hand.getSize(); ++i)
-            assertEquals(hand.returnValue(i), expectedFill);
         hand.reset();
         for (int i = 0; i < hand.getSize(); ++i)
             assertEquals(hand.returnValue(i), expectedHand.returnValue(i));
